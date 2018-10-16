c77a21a2f0727043fbeff00387dae740d779dba2
天川之约 <woaitianwen@qq.com>
2018-10-16 08:25:26 +0800

    增加角色实体类

---
+++ src/main/java/com/weiyu/domain/Role.java
@@ -0,0 +1,26 @@
+package com.weiyu.domain;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/12 下午4:36.
+ */
+public class Role {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
