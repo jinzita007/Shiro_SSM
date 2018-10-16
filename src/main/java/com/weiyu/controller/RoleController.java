09b3d9c0c96745bcfa4b8fd8150d1ade4c611d66
天川之约 <woaitianwen@qq.com>
2018-10-16 09:17:21 +0800

    增加角色控制层--跳转新增角色+查询角色信息

--- src/main/java/com/weiyu/controller/RoleController.java
+++ src/main/java/com/weiyu/controller/RoleController.java
@@ -1,21 +1,45 @@
 package com.weiyu.controller;
 
+import com.weiyu.domain.Role;
 import com.weiyu.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * @author zhouyulong
  * @date 2018/10/13 上午9:10.
  */
 @Controller
-@RequestMapping("/role")
+@RequestMapping("role")
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
 
+    /**
+     * 跳转新增角色页面
+     * @return
+     */
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String addRole() {
+        return "role/add";
+    }
 
+    /**
+     * 查询所有角色信息
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String RoleList(Model model) {
+        List<Role> roleList = roleService.findRoles();
+        model.addAttribute("roleList", roleList);
+        return "role/list";
+    }
 
 }
