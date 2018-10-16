8e0d5522e34e360895231ed373a1f41dedbad2f9
天川之约 <woaitianwen@qq.com>
2018-10-16 13:38:45 +0800

    权限数据层接口--查询所有权限

--- src/main/java/com/weiyu/dao/PermissionDao.java
+++ src/main/java/com/weiyu/dao/PermissionDao.java
@@ -2,10 +2,15 @@ package com.weiyu.dao;
 
 import com.weiyu.domain.Permission;
 
+import java.util.List;
+
 /**
  * @author zhouyulong
  * @date 2018/10/13 上午8:20.
  */
 public interface PermissionDao {
-    int save(Permission permission);
+
+    List<Permission> findAllPermission();
+    void addPermission(Permission permission);
+
 }
