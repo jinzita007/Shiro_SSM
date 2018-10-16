450750d1e5776b4aabb5399f2b33a922d09827cd
天川之约 <woaitianwen@qq.com>
2018-10-16 15:05:11 +0800

    修改角色权限实体类

--- src/main/java/com/weiyu/domain/RolePermission.java
+++ src/main/java/com/weiyu/domain/RolePermission.java
@@ -5,31 +5,31 @@ package com.weiyu.domain;
  * @date 2018/10/12 下午4:33.
  */
 public class RolePermission {
-    private Integer id;
-    private Integer role_id;
-    private Integer 	permission_id;
+    private Long id;
+    private Long roleId;
+    private Long permissionId;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Integer getRole_id() {
-        return role_id;
+    public Long getRoleId() {
+        return roleId;
     }
 
-    public void setRole_id(Integer role_id) {
-        this.role_id = role_id;
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
     }
 
-    public Integer getPermission_id() {
-        return permission_id;
+    public Long getPermissionId() {
+        return permissionId;
     }
 
-    public void setPermission_id(Integer permission_id) {
-        this.permission_id = permission_id;
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
     }
 }
