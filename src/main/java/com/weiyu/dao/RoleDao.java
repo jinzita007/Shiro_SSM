9babe20ebb7d6d8d6cf3cb4a88d7b724727c78e3
天川之约 <woaitianwen@qq.com>
2018-10-16 08:26:53 +0800

    增加角色数据层接口

---
+++ src/main/java/com/weiyu/dao/RoleDao.java
@@ -0,0 +1,15 @@
+package com.weiyu.dao;
+
+import com.weiyu.domain.Role;
+
+import java.util.List;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/13 上午8:19.
+ */
+public interface RoleDao {
+    int save(Role role);
+
+    List<Role> findRoles();
+}
