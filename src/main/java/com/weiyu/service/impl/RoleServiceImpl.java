146d3d5556b64efea62ca78d733e91a22b12dc31
天川之约 <woaitianwen@qq.com>
2018-10-16 15:42:44 +0800

    实现角色业务层--新增角色权限关联

--- src/main/java/com/weiyu/service/impl/RoleServiceImpl.java
+++ src/main/java/com/weiyu/service/impl/RoleServiceImpl.java
@@ -2,6 +2,7 @@ package com.weiyu.service.impl;
 
 import com.weiyu.dao.RoleDao;
 import com.weiyu.domain.Role;
+import com.weiyu.domain.RolePermission;
 import com.weiyu.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,11 +20,6 @@ public class RoleServiceImpl implements RoleService {
     private RoleDao roleDao;
 
     @Override
-    public void addRole(Role role){
-        roleDao.addRole(role);
-    }
-
-    @Override
     public List<Role> findRoles(){
         return roleDao.findRoles();
     }
@@ -31,5 +27,16 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public void deleteRole(Long id){
         roleDao.deleteRole(id);
+    }
+
+    @Override
+    public void addRole(Role role, Long... permissionIds) {
+        roleDao.addRole(role);
+        if(permissionIds != null && permissionIds.length>0 ) {
+            for (Long permissionId : permissionIds) {
+                System.out.println("--------角色id: "+role.getId()+"-----------角色权限ID: "+permissionId);
+                roleDao.addRolePermission(new RolePermission(role.getId(),permissionId));
+            }
+        }
     }
 }
