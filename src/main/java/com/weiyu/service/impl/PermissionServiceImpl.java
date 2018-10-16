fcc4012c31835c2594bae6b430d96578a05202ad
天川之约 <woaitianwen@qq.com>
2018-10-16 13:39:40 +0800

    创建实现权限业务层

---
+++ src/main/java/com/weiyu/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.weiyu.service.impl;
+
+import com.weiyu.service.PermissionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/16 下午1:37.
+ */
+@Service("permissionService")
+public class PermissionServiceImpl implements PermissionService {
+}
