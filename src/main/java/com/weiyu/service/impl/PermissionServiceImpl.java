d1fce2f64a171e0c2a165b35cb30c6a48f620bfe
天川之约 <woaitianwen@qq.com>
2018-10-16 14:12:30 +0800

    实现权限业务层--查询所有权限信息

--- src/main/java/com/weiyu/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/weiyu/service/impl/PermissionServiceImpl.java
@@ -1,7 +1,12 @@
 package com.weiyu.service.impl;
 
+import com.weiyu.dao.PermissionDao;
+import com.weiyu.domain.Permission;
 import com.weiyu.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author zhouyulong
@@ -9,4 +14,12 @@ import org.springframework.stereotype.Service;
  */
 @Service("permissionService")
 public class PermissionServiceImpl implements PermissionService {
+
+    @Autowired
+    private PermissionDao permissionDao;
+
+    @Override
+    public List<Permission> findAllPermission(){
+        return permissionDao.findAllPermission();
+    }
 }
