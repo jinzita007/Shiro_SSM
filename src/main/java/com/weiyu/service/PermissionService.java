f8704331cfca6e4cd9f3e070b2037605be3fc7dc
天川之约 <woaitianwen@qq.com>
2018-10-16 14:12:10 +0800

    权限业务层接口--查询所有权限信息

--- src/main/java/com/weiyu/service/PermissionService.java
+++ src/main/java/com/weiyu/service/PermissionService.java
@@ -1,9 +1,17 @@
 package com.weiyu.service;
 
+import com.weiyu.domain.Permission;
+
+import java.util.List;
+
 /**
  * @author zhouyulong
  * @date 2018/10/16 下午1:36.
  */
 public interface PermissionService {
-
+    /**
+     * 查询所有权限信息
+     * @return
+     */
+    List<Permission> findAllPermission();
 }
