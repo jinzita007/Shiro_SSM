244dabaebca74c754a23f8d8089634d9288b9012
天川之约 <woaitianwen@qq.com>
2018-10-16 08:22:48 +0800

    增加权限控制器

---
+++ src/main/java/com/weiyu/dao/PermissionDao.java
@@ -0,0 +1,11 @@
+package com.weiyu.dao;
+
+import com.weiyu.domain.Permission;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/13 上午8:20.
+ */
+public interface PermissionDao {
+    int save(Permission permission);
+}
