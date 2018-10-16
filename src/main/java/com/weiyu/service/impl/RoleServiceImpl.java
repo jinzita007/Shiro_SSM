d39d2d1875af371512f35032a415641b1f62244b
天川之约 <woaitianwen@qq.com>
2018-10-16 08:28:18 +0800

    实现角色业务层

---
+++ src/main/java/com/weiyu/service/impl/RoleServiceImpl.java
@@ -0,0 +1,30 @@
+package com.weiyu.service.impl;
+
+import com.weiyu.dao.RoleDao;
+import com.weiyu.domain.Role;
+import com.weiyu.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/13 上午9:07.
+ */
+
+@Service("roleService")
+public class RoleServiceImpl implements RoleService {
+    @Autowired
+    private RoleDao roleDao;
+
+    @Override
+    public int save(Role role) {
+        return roleDao.save(role);
+    }
+
+    @Override
+    public List<Role> findRoles(){
+        return roleDao.findRoles();
+    }
+}
