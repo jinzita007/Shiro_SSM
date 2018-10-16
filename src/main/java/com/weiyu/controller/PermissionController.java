ad8de6eed8d9480b60fc1cfdd30e44e1e3b91ee1
天川之约 <woaitianwen@qq.com>
2018-10-16 13:39:18 +0800

    创建权限业务层接口

--- src/main/java/com/weiyu/controller/PermissionController.java
+++
@@ -1,9 +0,0 @@
-package com.weiyu.controller;
-
-/**
- * @author zhouyulong
- * @date 2018/10/16 下午1:26.
- * 权限控制层
- */
-public class PermissionController {
-}
---
+++ src/main/java/com/weiyu/service/PermissionService.java
@@ -0,0 +1,9 @@
+package com.weiyu.service;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/16 下午1:36.
+ */
+public interface PermissionService {
+
+}
